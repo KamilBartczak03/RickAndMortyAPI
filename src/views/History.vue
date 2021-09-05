d8c464745d984c0f1a789cdbec43289ba4cc65ac
Kamil Bartczak <KamilBartczak2021@gmail.com>
2021-09-05 15:07:57 +0200

    Create accordion message layout

--- src/views/History.vue
+++ src/views/History.vue
@@ -2,7 +2,19 @@
   <h1 class="page-header">Message history</h1>
   <div class="about" style="padding: 0 300px;">
     <im-accordion>
-      content
+      <div class="message">
+        <div class="message__details">
+          <img class="message__details-avatar" src="https://source.unsplash.com/random">
+          <div class="message__details-text">
+            <span class="message__details-span">Sent to: Rick Sanchez</span>
+            <span class="message__details-span">Date: 01.01.2021</span>
+            <span class="message__details-span">At: 21:37</span>
+          </div>
+        </div>
+        <div class="message__text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, asperiores. Rem cum hic, incidunt ducimus eum blanditiis praesentium, quibusdam quo, assumenda officia laboriosam eius tenetur exercitationem accusantium cupiditate temporibus itaque.
+        </div>
+      </div>
     </im-accordion>
 
     <im-accordion>
@@ -24,3 +36,5 @@ import imAccordion from "@/components/ui/im-accordion.vue"
     margin-top: 40px;
   }
 </style>
+
+<style lang="scss" scoped src="@/components/ui/message.scss"></style>
